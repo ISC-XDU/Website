0953c3953930a0c0c0657428851416d348b32737
Inspur Web Team <web@inspur-club.local>
2026-09-04 22:18:10 +0800

    feat: dark mode toggle (3D box style) + dark mode base styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss';
 import typography from '@tailwindcss/typography';
 
 const config: Config = {
+  darkMode: ['selector', '[data-theme="dark"]'],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
